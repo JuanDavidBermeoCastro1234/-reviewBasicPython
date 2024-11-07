9b9e61f77d893a3d6eec637316038d1d5fcfdbcf
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-07 13:21:46 -0500

    Ejercicio 2: Calificación de una nota

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numero=int(input("ingrese un numero entero po0r favor : "))
-if numero%2==0:
-    print(f"el numero es par {numero} ")
-else:
-    print(f"no es par {numero}")
+# Ejercicio 2: Calificación de una nota
+# Escribe un programa que determine si una nota numérica es "Aprobado" o "Reprobado" usando
+# if .
+# Enunciado:
+# Solicita al usuario una calificación y determina si la nota es aprobatoria (>= 60) o reprobatoria (<
+# 60).
+nota=float(input("enter your note please: "))
+if nota>=60 :
+    print(f"aprovate {nota}")
+else :
+    print(f" dont aprovate {nota}")
+    
