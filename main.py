45b1b3ff594ea58c59dc372e7f58f0f84e5af39b
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 16:31:33 -0500

    exersice 12

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-# Ejercicio 11: Conversión de temperaturas
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+# Ejercicio 12: Calculadora de IMC (Índice de Masa Corporal)
+# Escribe un programa que calcule el IMC y determine el estado de peso.
+# Enunciado:
+# Solicita al usuario su peso (en kg) y su altura (en metros). Calcula el IMC y clasifícalo en bajo peso
+# (<18.5), peso normal (18.5-24.9), sobrepeso (25-29.9), o obesidad (>=30).
 
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
 
-# Ask the user for temperature and scale
-temperature = float(input("Enter a temperature: "))
-scale = input("Enter a scale (C for Celsius, F for Fahrenheit): ").lower()
 
-# Use match to perform the conversion
-match scale:
-    case "f":
-        # Convert from Celsius to Fahrenheit
-        fahrenheit_value = celsius_to_fahrenheit(temperature)
-        print(f"The temperature {temperature}°C is equal to {fahrenheit_value}°F")
-    case "c":
-        # Convert from Fahrenheit to Celsius
-        celsius_value = fahrenheit_to_celsius(temperature)
-        print(f"The temperature {temperature}°F is equal to {celsius_value}°C")
-    case _:
-        print("Invalid scale. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+weight=float(input("Please enter your weight :"))
+height=float(input("Please enter your height : "))
+
+imc=weight/height**2
+
+if imc<18.5:
+    print(f"underweight")
+elif imc>18.5 and imc<24.9:
+    print(f"low normal")
+elif imc>25 and imc<29.9:
+    print(f"overweight")
+elif imc>30:
+    print(f"obesity")
