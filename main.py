7adfea9e0f0eec22cda074e7a647d54d24e1c491
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 16:32:16 -0500

    exercise 14

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# Ejercicio 13: Comparación de tres números
-# Escribe un programa que determine el mayor de tres números usando if .
+# Ejercicio 14: Adivinanza de letras
+# Escribe un programa que permita al usuario adivinar una letra secreta usando match .
 # Enunciado:
-# Solicita al usuario que ingrese tres números y determina cuál es el mayor.
+# El programa contiene una letra secreta (por ejemplo, "A"). El usuario debe adivinar la letra, y el
+# programa le indicará si acertó o no.
 
-number1=int(input("Enter a number : "))
-number2=int(input("Enter a number : "))
-number3=int(input("Enter a number : "))
+value = input("Enter a value (A, B, C): ").lower()
 
-if number1>number2 and number1>number3:
-    print(f"{number1} is older")
-elif number2>number1 and number2>number3:
-    print(f"{number2} is older")    
-elif number3>number1 and number3>number2:
-    print(f"{number3} is older")
-else :
-    print("The three numbers are equal")
+if value == "a":
+    print("You chose A")
+elif value == "b":
+    print("You chose B")
+elif value == "c":
+    print("You chose C")
+else:
+    print("Invalid value")
