27802ea036e79d83225f301b61d7adb39264cb90
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 13:01:44 -0500

    exercise 5

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# Solicitar dos números y la operación a realizar
-# Ejercicio 4: Determinación del tipo de triángulo
-# Escribe un programa que determine el tipo de triángulo en función de sus lados usando if .
+# Ejercicio 5: Días de la semana
+# Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
+# semana usando match .
 # Enunciado:
-# Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el
-# triángulo es equilátero, isósceles o escaleno.
-   
-lado1=float(input(" Enter the first side of the triangle : "))
-lado2=float(input(" Enter the second side of the triangle : "))
-lado3=float(input(" Enter the third side of the triangle : "))
+# Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
+# 7 = Domingo).
+import math
 
-if lado1==lado2==lado3:
-    print(f"the triangle is equilateral ")
-elif lado1!= lado2!=lado3:
-    print(f"the triangle is escaleno  ")
-else:
-    print("the triangle is isoceles ")
+days_of_the_week=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+day=int(input("What day of the week do you want to see? Enter the number : "))
+
+print(days_of_the_week [day-1])
