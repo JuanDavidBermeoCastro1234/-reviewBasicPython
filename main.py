f5638fc0cb0f8e060237d56aebcb69de77f7ec8b
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 13:42:27 -0500

    exercise 8

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-# Ejercicio 7: Número positivo, negativo o cero
-# Escribe un programa que determine si un número es positivo, negativo o cero usando if .
+# Ejercicio 8: Determinación de año bisiesto
+# Escribe un programa que determine si un año es bisiesto o no.
 # Enunciado:
-# Solicita al usuario que ingrese un número y determina si es positivo, negativo o cero.
+# Solicita al usuario que ingrese un año y determina si es bisiesto (divisible entre 4, pero no entre
+# 100, salvo que sea divisible entre 400).
 
-number=float(input("Enter a number : "))
+year=int(input("enter a year please : "))
 
-if number<0:
-    print(f"number is negative ")
-elif number>0:
-    print(f"number is positive")
-elif number==0:
-    print(f"number equals zero") 
+if year%4==0 and year!=0:
+    print(F"is year bleap year ")
+elif year%400==0:
+    print(f"is year bleap year")
+else:
+    print(f"non leap year ")
