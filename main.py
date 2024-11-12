6057d086b076c1d738eb08501eaf3e67b235b3ed
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 16:05:43 -0500

    exercise 6

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# Ejercicio 10: Clasificación de notas
-# Escribe un programa que asigne una calificación basada en una nota numérica.
+# Ejercicio 6: Juego de adivinanza de números
+# Escribe un programa que implemente un juego de adivinanza de números.
 # Enunciado:
-# Solicita una nota numérica y clasifícala como A (90-100), B (80-89), C (70-79), D (60-69), o F (<60).
+# El programa genera un número aleatorio entre 1 y 10. El usuario debe adivinar el número, y el
+# programa indica si el número ingresado es mayor, menor o igual al número generado
 
-note=int(input("Please enter your note : "))
+import random
 
-if note>=90 and note<=100:
-    print(" is note A")
-elif note>=80 and note<=89:
-    print("note is B")
-elif note>=70 and note<=79:
-    print("note is C")
-elif note>=60 and note<=69:
-    print("note is D")
-elif note<60:
-    print("note is F")
+# Generar un número aleatorio entre 1 y 100 (Este es el número que solo tú sabrás)
+random_number = random.randint(1, 100)
+
+# Imprimir el número para que solo tú lo veas
+#print(f"(El número aleatorio generado para adivinar es: {random_number})")
+
+# Pedir un número al usuario
+number = int(input("Ingresa un número entero, por favor: "))
+
+# Mientras el número ingresado no sea el correcto, seguir pidiendo
+while random_number != number:
+    print("No has adivinado el número, sigue intentando.")
+    number = int(input("Intenta con otro número: "))
+
+# Si el número es correcto
+print(f"¡Felicidades! Has adivinado el número.")
