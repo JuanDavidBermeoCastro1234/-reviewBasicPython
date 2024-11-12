e22ffba0bb6055edc73f0a76bc1a550c6456225b
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 16:51:58 -0500

    exercise 15

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# Ejercicio 14: Adivinanza de letras
-# Escribe un programa que permita al usuario adivinar una letra secreta usando match .
+# Ejercicio 15: Cálculo del salario neto
+# Escribe un programa que calcule el salario neto de un empleado después de aplicar impuestos.
 # Enunciado:
-# El programa contiene una letra secreta (por ejemplo, "A"). El usuario debe adivinar la letra, y el
-# programa le indicará si acertó o no.
+# Solicita al usuario su salario bruto y su país de residencia. Calcula el salario neto aplicando
+# impuestos: el 20% para residentes de "País A", el 15% para "País B" y el 10% para "País C". Si el
+# país no está en la lista, aplica un 25% de impuestos
 
-value = input("Enter a value (A, B, C): ").lower()
 
-if value == "a":
-    print("You chose A")
-elif value == "b":
-    print("You chose B")
-elif value == "c":
-    print("You chose C")
+salary = float(input("Enter your salary: "))
+country_b = salary / 0.15
+country_a = salary / 0.20
+country_c = salary / 0.10
+other_country = salary / 0.25
+
+country = input("Enter your country (a, b, c): ").lower()
+
+if country == "a":
+    print(f"Your salary after tax is {country_a}")
+elif country == "b":
+    print(f"Your salary after tax is {country_b}")
+elif country == "c":
+    print(f"Your salary after tax is {country_c}")
 else:
-    print("Invalid value")
+    print(f"Your salary after tax is {other_country}")
