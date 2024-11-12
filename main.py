607356e24d4210f5327aff65d700ba7ff35bf2ed
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-11-12 13:35:54 -0500

    exersice 7

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-# Ejercicio 5: Días de la semana
-# Escribe un programa que, dado un número del 1 al 7, imprima el día correspondiente de la
-# semana usando match .
+# Ejercicio 7: Número positivo, negativo o cero
+# Escribe un programa que determine si un número es positivo, negativo o cero usando if .
 # Enunciado:
-# Solicita al usuario un número del 1 al 7 y muestra el día de la semana correspondiente (1 = Lunes,
-# 7 = Domingo).
-import math
+# Solicita al usuario que ingrese un número y determina si es positivo, negativo o cero.
 
-days_of_the_week=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+number=float(input("Enter a number : "))
 
-day=int(input("What day of the week do you want to see? Enter the number : "))
-
-print(days_of_the_week [day-1])
+if number<0:
+    print(f"number is negative ")
+elif number>0:
+    print(f"number is positive")
+elif number==0:
+    print(f"number equals zero") 
